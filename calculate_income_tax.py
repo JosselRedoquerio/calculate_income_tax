91ae5e9ddac480d505ce4fd958cca17fcbf0dbc4
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-23 13:24:10 +0800

    tax payable

--- calculate_income_tax.py
+++ calculate_income_tax.py
@@ -1,1 +1,13 @@
 # Exercise 12: Calculate income tax for the given income by adhering to the rules below
+
+income = 120000
+tax_payable = 0
+print("Your income", income)
+
+if income <= 5000:
+    tax_payable = 0
+elif income <= 10000:
+    # no tax on first 5,000
+    x = income - 5000
+    # 10% tax
+    tax_payable = x * 10 / 100
