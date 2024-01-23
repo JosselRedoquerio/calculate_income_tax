8cda6b27df5e5323a45bc6291456c11c7d6138e3
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-23 13:31:17 +0800

    printing tax

--- calculate_income_tax.py
+++ calculate_income_tax.py
@@ -1,13 +1,24 @@
 # Exercise 12: Calculate income tax for the given income by adhering to the rules below
 
-income = 120000
+income = 150000
 tax_payable = 0
-print("Your income", income)
+print("Your income:", income)
 
 if income <= 5000:
     tax_payable = 0
 elif income <= 10000:
-    # no tax on first 5,000
+    # first 5,000 no tax
     x = income - 5000
     # 10% tax
     tax_payable = x * 10 / 100
+else:
+    # first 5,000
+    tax_payable = 0
+
+    # next 10,000 10% tax
+    tax_payable = 10000 * 10 / 100
+
+    # remaining 20%tax
+    tax_payable += (income - 20000) * 20 / 100
+
+print("Total tax to pay is:", tax_payable)
